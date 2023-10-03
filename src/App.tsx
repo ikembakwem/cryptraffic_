ee5115d37ed91322a4a58a077ac3119d03af3c4b
Ikechukwu Mbakwem <ikembakwem@gmail.com>
2023-10-03 10:11:28 +0100

    Initial development setup for Vite, React and TailwindCSS frontend project

---
+++ src/App.tsx
@@ -0,0 +1,10 @@
+function App() {
+  return (
+    <section className=" bg-blue-200 h-screen">
+      <div className="flex items-center justify-center font-bold text-4xl text-teal-800">
+        Hello World!
+      </div>
+    </section>
+  );
+}
+export default App;
